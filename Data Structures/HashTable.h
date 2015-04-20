05e5e5b9a30f866316becfe41ae0e7928194beb5
Diogo Constancio <drconstancio@hotmail.com>
2015-04-20 18:45:44 +0100

    Fixed queries, added hashtable
    data in hashtable is broken

---
+++ Data Structures/HashTable.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct hashTable *HashTable;
+
+typedef struct slot *Slot;
+
+HashTable initHashTable(int size);
+
+HashTable insertHashTable(HashTable t, char *key, void* data, int dataSize);
+
+Slot getSlot(HashTable table, char *key);
+
+void *getSlotData(Slot e);
+
+int getSlotDataSize(Slot e);
+
+Slot setSlotData(Slot s, void *data, int dataSize);
